ef130d88ce2e926add858005ff1034f5fadd08de
MinhTuyen <tuyen.nguyen@yedda.io>
2020-04-25 13:16:40 +0700

    Updates

--- precache-manifest.3ac184b16121bb9c77ed94b4d2f4293e.js
+++ precache-manifest.3ac184b16121bb9c77ed94b4d2f4293e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "64affbc7ea1770f7e53ea217f99715b3",
+    "revision": "51ad8aef2d6a171cdc3c66264b7f788a",
     "url": "/Duyanh/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Duyanh/static/css/2.076a1fae.chunk.css"
   },
   {
-    "revision": "459c2f86b41d963bda70",
+    "revision": "9e9a29845e3a4718a847",
     "url": "/Duyanh/static/css/main.d1b05096.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Duyanh/static/js/2.40d676d9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "459c2f86b41d963bda70",
-    "url": "/Duyanh/static/js/main.2d45a635.chunk.js"
+    "revision": "9e9a29845e3a4718a847",
+    "url": "/Duyanh/static/js/main.bff688ac.chunk.js"
   },
   {
     "revision": "1fa909ad3a8241d03db9",
